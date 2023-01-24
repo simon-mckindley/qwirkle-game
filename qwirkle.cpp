6cbfe51f8ecb915ea096b8c95785e5575a268475
Simon Mckindley <S9406133@student.rmit.edu.au>
2023-01-25 02:15:27 +1100

    Gameplay framework and UserPrompt w min checking

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -9,7 +9,11 @@ void mainMenuOption(std::string userSelection);
 void createNewGame();
 void loadGame();
 void printCredits();
+std::string getPlayerName();
 std::string getInput();
+void gamePlay();
+bool gamePlayOption();
+void printGameStatus();
 
 #define EXIT_SUCCESS 0
 
@@ -21,7 +25,7 @@ std::string getInput();
 
 int main(void)
 {
-    std::string userSelection = "";
+    std::string userSelection;
 
     std::cout << "\nWelcome to Qwirkle!"
               << "\n-------------------"
@@ -81,30 +85,32 @@ void mainMenuOption(std::string userSelection)
 
 void createNewGame()
 {
-    // TODO
     std::cout << "\nStarting a new game." << std::endl;
 
     std::string player1;
     std::string player2;
     std::cout << "\nEnter a name for Player 1 (Uppercase characters only)" << std::endl;
-    player1 = getInput();
+    player1 = getPlayerName();
     std::cout << "\nEnter a name for Player 2 (Uppercase characters only)" << std::endl;
-    player2 = getInput();
+    player2 = getPlayerName();
     std::cout << "\nWelcome, " << player1 << " and " << player2
               << "\n\tLets Play!";
 
-    std::cout << "\n.... game play starts" << std::endl;
+    // TODO Initialise new game 2.3.10
+
+    gamePlay();
 }
 
 void loadGame()
 {
-    // TODO
     std::cout << "\nLoad a saved game" << std::endl;
 
     std::string filename;
     std::cout << "\nEnter the filename from which to load a save game" << std::endl;
     filename = getInput();
     std::cout << "\nLoading game from > " << filename << std::endl;
+
+    // TODO Load game from file 2.2.2
 
     std::cout << "\n.... game play starts" << std::endl;
 }
@@ -144,10 +150,129 @@ void printCredits()
     std::cout << "------------------------------" << std::endl;
 }
 
+// Accepts and validates player names
+std::string getPlayerName()
+{
+    std::string name = "";
+    bool valid;
+    do
+    {
+        valid = true;
+        name = getInput();
+        for (char c : name)
+        {
+            if (!isupper(c))
+            {
+                valid = false;
+                std::cout << "Invalid input" << std::endl;
+                break;
+            }
+        }
+
+    } while (!valid);
+
+    return name;
+}
+
+// General purpose user input function
 std::string getInput()
 {
     std::string userSelection;
     std::cout << "> ";
-    std::cin >> userSelection;
+    std::getline(std::cin, userSelection);
+
+    if (std::cin.eof())
+    {
+        // Exit for testing purposes
+        std::cout << "\nGoodbye" << std::endl;
+        exit(EXIT_SUCCESS);
+    }
+
     return userSelection;
+}
+
+// Main gameplay function
+void gamePlay()
+{
+    bool endGame = false;
+
+    do
+    {
+        std::cout << "\n\n<Player>"
+                  << ", it's your turn";
+        printGameStatus();
+        endGame = gamePlayOption();
+
+    } while (!endGame);
+}
+
+// Gets and validates user gameplay input
+bool gamePlayOption()
+{
+    bool invalid;
+    bool endGame = false;
+    std::string userInput;
+    do
+    {
+        invalid = false;
+        userInput = getInput();
+        int pos = userInput.find_first_of(" ");
+        if (pos != std::string::npos)
+        {
+            // Valid input: place, replace, save
+            std::string cmd = userInput.substr(0, pos);
+            if (cmd == "place")
+            {
+                std::string tile = userInput.substr(pos + 1, 2);
+                std::string location = userInput.substr(pos + 7);
+                std::cout << "Place:" << tile << ":" << location << std::endl;
+                // TODO validate tile and location
+            }
+            else if (cmd == "replace")
+            {
+                std::string tile = userInput.substr(pos + 1, 2);
+                std::cout << "Replace:" << tile << std::endl;
+                // TODO validate tile
+            }
+            else if (cmd == "save")
+            {
+                std::string fileName = userInput.substr(pos + 1);
+                std::cout << "File:" << fileName << std::endl;
+            }
+            else
+            {
+                std::cout << "Invalid input" << std::endl;
+                invalid = true;
+            }
+        }
+        else if (userInput == "quit")
+        {
+            endGame = true;
+        }
+        else
+        {
+            std::cout << "Invalid input" << std::endl;
+            invalid = true;
+        }
+
+    } while (invalid);
+
+    return endGame;
+}
+
+// Prints the game status a the start of each players turn
+void printGameStatus()
+{
+    std::cout << "\nScore for "
+              << "<Player1>"
+              << ": "
+              << "<9>";
+    std::cout << "\nScore for "
+              << "<Player2>"
+              << ": "
+              << "<9>";
+    std::cout << "\nPRINT BOARD HERE" << std::endl;
+    std::cout << "\nYour hand is:\n"
+              << "<hand>\n"
+              << std::endl;
 }
